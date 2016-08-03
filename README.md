d8a753cd6b43d7fff1ebfa7342989285ab7c4f1c
wangyingtaodeepin <wangyingtao@deepin.com>
2016-08-03 17:53:43 +0800

    add basic test for position left and right

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ download from https://github.com/wangyingtaodeepin/dogtail
 ## install packages
 > sudo apt-get install python3-pyatspi python3-pip libwnck-3-dev  
 > sudo pip3 install PyUserInput
+
+## run the test
+> python3 dde-dock.py
+
+## about the result.txt
+The result is saved to the current file result.txt.  
+Every line has the testlink case id and the test result.
+The result file is aimed for posting to testlink.
---
+++ testFashionExistRight.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+from time import sleep
+from lib import utils
+from lib import runner
+
+result = True
+
+class MyTestResult(runner.MyTextTestResult):
+    def addError(self, test, err):
+        super(MyTestResult, self).addError(test, err)
+        global result
+        result = result and False
+
+    def addFailure(self, test, err):
+        super(MyTestResult, self).addFailure(test, err)
+        global result
+        result = result and False
+
+class FashionExistRight(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.caseid = '68461'
+        cls.casename = "all-2484:时尚模式右方显示测试"
+        cls.ddedockobject = utils.getDdeDockObject()
+        cls.defaultdisplaymode = utils.getDdeDockDisplayMode()
+        cls.defaultposition = utils.getDdeDockPosition()
+        cls.dock_mainwindow = "dock-mainwindow"
+
+    @classmethod
+    def tearDownClass(cls):
+        global result
+        utils.commitresult(cls.caseid, result)
+
+        if utils.getDdeDockDisplayMode() != cls.defaultdisplaymode:
+            utils.setDdeDockDisplayMode(cls.defaultdisplaymode)
+
+        if utils.getDdeDockPosition() != cls.defaultposition:
+            utils.setDdeDockPosition(cls.defaultposition)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testBasicFunction(self):
+        defaultdisplaymode = utils.getDdeDockDisplayMode()
+        self.assertTrue(utils.dock.displaymode_fashion == defaultdisplaymode)
+        defaultposition = utils.getDdeDockPosition()
+        self.assertTrue(utils.dock.position_bottom == defaultposition)
+        defaulthidestate = utils.getDdeDockHideState()
+        self.assertTrue(utils.dock.hidestate_show == defaulthidestate)
+        main_window = self.ddedockobject.child(self.dock_mainwindow)
+        (width, height) = main_window.size
+        self.assertTrue(width > 0)
+        self.assertTrue(height > 0)
+
+    def testChangePositionToRight(self):
+        utils.m.click(int(utils.resolution.width/2), utils.resolution.height, 2)
+        sleep(2)
+        utils.keySingle(utils.k.down_key)
+        utils.keySingle(utils.k.left_key)
+        utils.keySingle(utils.k.down_key)
+        utils.keySingle(utils.k.down_key)
+        utils.keySingle(utils.k.down_key)
+        utils.keySingle(utils.k.down_key)
+        utils.keySingle(utils.k.enter_key)
+        defaultdisplaymode = utils.getDdeDockDisplayMode()
+        self.assertTrue(utils.dock.displaymode_fashion == defaultdisplaymode)
+        defaultposition = utils.getDdeDockPosition()
+        self.assertTrue(utils.dock.position_right == defaultposition)
+        defaulthidestate = utils.getDdeDockHideState()
+        self.assertTrue(utils.dock.hidestate_show == defaulthidestate)
+        main_window = self.ddedockobject.child(self.dock_mainwindow)
+        (width, height) = main_window.size
+        self.assertTrue(width > 0)
+        self.assertTrue(height > 0)
+        (x, y) = main_window.position
+        self.assertTrue((utils.resolution.width - width) == x)
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(FashionExistRight('testBasicFunction'))
+    suite.addTest(FashionExistRight('testChangePositionToRight'))
+    return suite
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(resultclass=MyTestResult).run(suite())
